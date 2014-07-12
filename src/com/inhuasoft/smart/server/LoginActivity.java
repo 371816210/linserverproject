f48d76c7b72a16d2ec07473b6a8f7bdd62da89e4
zwzhu <371816210@163.com>
2014-07-12 12:47:19 +0800

    set tcp as default

--- src/com/inhuasoft/smart/server/LoginActivity.java
+++ src/com/inhuasoft/smart/server/LoginActivity.java
@@ -155,6 +155,8 @@ public class LoginActivity extends Activity implements OnClickListener {
 		 if(LinphonePreferences.instance().getAccountCount() < 1)
 		 {
 		   LinphonePreferences.instance().CreatedAccount(getDeviceNo(),getDeviceNo(),"115.28.9.71");
+		   LinphonePreferences.instance().setInitiateVideoCall(true);
+		   LinphonePreferences.instance().setAutomaticallyAcceptVideoRequests(true);
 		 }
 		 
 	}
